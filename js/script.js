9dce5c15f3838eeb39d91b4917c53681fe175ae3
DevVitorPedra <75690532+DevVitorPedra@users.noreply.github.com>
2021-12-22 14:13:16 -0300

    refactoring/knotflix

--- js/script.js
+++ js/script.js
@@ -14,3 +14,22 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+const btnPlay =  document.getElementById('play')
+const audio = document.getElementById('audio')
+const playBtn = document.getElementById('icon')
+let playStop = 0
+btnPlay.addEventListener('click',()=>{
+    if(playStop ===0){
+        audio.play()
+        playStop=1
+            playBtn.classList.remove('bi-play-fill')
+            playBtn.classList.add('bi-pause-circle')
+    }else{
+        audio.pause()
+        playStop =0
+        playBtn.classList.add('bi-play-fill')
+        playBtn.classList.remove('bi-pause-circle')
+
+    }
+})
